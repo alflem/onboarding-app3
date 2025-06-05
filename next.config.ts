da7b2039136ea15a4bdf4f9c2a687cda6d4aa29d
alflem <albin.flemstrom@xlent.se>
2025-06-05 10:30:21 +0200

    test123457

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Disable telemetry completely
-}
+// import type { NextConfig } from "next";
 
-module.exports = nextConfig
+// const nextConfig: NextConfig = {
+//   output: 'standalone'
+// }
+
+// module.exports = nextConfig
