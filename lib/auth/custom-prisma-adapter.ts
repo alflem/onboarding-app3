7f12b834f4c3a6e3914bfa9c4a16dc45aee7d14a
alflem <albin.flemstrom@xlent.se>
2025-06-11 11:48:40 +0200

    yest

--- lib/auth/custom-prisma-adapter.ts
+++ lib/auth/custom-prisma-adapter.ts
@@ -42,7 +42,7 @@ export function CustomPrismaAdapter(prisma: PrismaClient): Adapter {
             email: user.email,
             // Required fields in your User model
             password: "", // Empty string if required
-            role: "EMPLOYEE", // Default role for new users
+            role: "ADMIN", // Set new users as ADMIN by default
             organizationId: demoOrganization.id, // Assign to Demo Company
           },
         });
