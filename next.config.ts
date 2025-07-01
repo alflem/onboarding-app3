2a0a5313dafd2022d35d40fef0db67f37e63c67f
alflem <albin.flemstrom@xlent.se>
2025-07-01 15:13:30 +0200

    Add Prisma plugin to Next.js configuration for improved server-side support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
